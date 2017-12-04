1272acfb9f6032e5926cb98c40807f4723dba010
jintaoguan <jintaoguan0817@gmail.com>
2017-12-04 01:47:03 -0800

    add more comments for BufferPool

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/BufferPool.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/BufferPool.java
@@ -41,13 +41,25 @@ import org.apache.kafka.common.utils.Time;
  * buffers are deallocated.
  * </ol>
  */
+/**
+ * ByteBuffer 的创建与释放非常消耗资源, 为了实现内存的高效利用, Kafka 客户端使用 BufferPool 来实现 ByteBuffer 的复用.
+ * 每个 BufferPool 对象只针对特定大小的 (poolableSize 变量指定) ByteBuffer 进行管理, 其他大小的 ByteBuffer 并不会缓存进 BufferPool.
+ * 一般情况下, 我们通过调节 RecordAccumulator.batchSize 变量, 使一个 MemoryRecords 对象可以存储多个 record.
+ * 但是如果一条 record 过大, 超过 MemoryRecords, 那么就不会复用 BufferPool 中缓存的 ByteBuffer,
+ * 而是额外单独分配 ByteBuffer, 使用完之后也不会放入 BufferPool 进行管理, 而是直接丢弃给 GC.
+ */
 public final class BufferPool {
 
+    // 整个 BufferPool 的大小
     private final long totalMemory;
+    // ByteBuffer 的大小, BufferPool 管理的所有的 ByteBuffer 的大小都是 poolableSize
     private final int poolableSize;
     private final ReentrantLock lock;
+    // 一个 ArrayDeque<ByteBuffer> 队列, 缓存的空闲的相同大小的 ByteBuffer,
     private final Deque<ByteBuffer> free;
+    // 所有阻塞线程的 Condition 对象
     private final Deque<Condition> waiters;
+    // 剩余的可用空间大小
     private long availableMemory;
     private final Metrics metrics;
     private final Time time;
@@ -96,24 +108,34 @@ public final class BufferPool {
                                                + this.totalMemory
                                                + " on memory allocations.");
 
+        // 加锁同步
         this.lock.lock();
         try {
             // check if we have a free buffer of the right size pooled
+            // 请求的是 poolableSize 指定大小的 ByteBuffer, 并且 free 中有空闲的 ByteBuffer
+            // 就从 free 中拿出空闲的 ByteBuffer.
             if (size == poolableSize && !this.free.isEmpty())
                 return this.free.pollFirst();
 
             // now check if the request is immediately satisfiable with the
             // memory on hand or if we need to block
+            // 当申请的空间大小不是 poolableSize, 则进行下面的处理
+            // 这一步计算 free 所有的 ByteBuffer 的总空间大小
             int freeListSize = this.free.size() * this.poolableSize;
+            // 可用空间 + 空闲的 ByteBuffer 的总空间大小满足请求的空间大小
             if (this.availableMemory + freeListSize >= size) {
                 // we have enough unallocated or pooled memory to immediately
                 // satisfy the request
+                // 为了让 availableMemory > size, freeUp() 方法会从 free 队列中不断释放 ByteBuffer,
+                // 直到 availableMemory 满足这次申请
                 freeUp(size);
                 this.availableMemory -= size;
                 lock.unlock();
+                // 这里没有使用 free 队列中的 ByteBuffer, 而是直接分配了 size 大小的 HeapByteBuffer.
                 return ByteBuffer.allocate(size);
             } else {
                 // we are out of memory and will have to block
+                // 没有足够的剩余空间, 需要阻塞在这里等待空间被释放
                 int accumulated = 0;
                 ByteBuffer buffer = null;
                 Condition moreMemory = this.lock.newCondition();
@@ -205,11 +227,14 @@ public final class BufferPool {
         lock.lock();
         try {
             if (size == this.poolableSize && size == buffer.capacity()) {
+                // 如果释放的 ByteBuffer 的大小是 poolableSize, 则放入 free 队列中进行管理.
                 buffer.clear();
                 this.free.add(buffer);
             } else {
+                // 如果释放的 ByteBuffer 的大小不是 poolableSize, 则不会复用这个 ByteBuffer, 仅仅更新可用空间大小.
                 this.availableMemory += size;
             }
+            // 唤醒一个因空间不足而阻塞的线程
             Condition moreMem = this.waiters.peekFirst();
             if (moreMem != null)
                 moreMem.signal();
@@ -225,6 +250,7 @@ public final class BufferPool {
     /**
      * the total free memory both unallocated and in the free list
      */
+    // 计算总可用空间大小
     public long availableMemory() {
         lock.lock();
         try {
@@ -237,6 +263,7 @@ public final class BufferPool {
     /**
      * Get the unallocated memory (not in the free list or in use)
      */
+    // 计算尚未分配的可用空间大小
     public long unallocatedMemory() {
         lock.lock();
         try {
@@ -249,6 +276,7 @@ public final class BufferPool {
     /**
      * The number of threads blocked waiting on memory
      */
+    // 阻塞的线程数量
     public int queued() {
         lock.lock();
         try {
@@ -268,6 +296,7 @@ public final class BufferPool {
     /**
      * The total memory managed by this pool
      */
+    // KafkaProducer 对象的 buffer 总大小
     public long totalMemory() {
         return this.totalMemory;
     }
