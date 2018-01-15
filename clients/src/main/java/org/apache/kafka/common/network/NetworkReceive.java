ce730f5f70465508e7afc169008b8c8c8d67e0a7
jintaoguan <jintaoguan0817@gmail.com>
2018-01-14 23:20:15 -0800

    add comments for NetworkReceive

--- clients/src/main/java/org/apache/kafka/common/network/NetworkReceive.java
+++ clients/src/main/java/org/apache/kafka/common/network/NetworkReceive.java
@@ -21,6 +21,10 @@ import java.nio.channels.ScatteringByteChannel;
 /**
  * A size delimited Receive that consists of a 4 byte network-ordered size N followed by N bytes of content
  */
+// NetworkReceive 是对一个网络接收到的请求的描述, 有三部分组成
+// size 是一个固定 4 字节的 ByteBuffer, 表示一个网络请求的具体内容的长度, 可以通过 size.getInt() 取得
+// buffer 是一个长度不固定的 ByteBuffer, 初始化时由 size.getInt() 作为参数申请空间, 里面存放了请求的具体内容
+// source 表示从哪里接收到的请求
 public class NetworkReceive implements Receive {
 
     public final static String UNKNOWN_SOURCE = "";
@@ -67,6 +71,7 @@ public class NetworkReceive implements Receive {
         return !size.hasRemaining() && !buffer.hasRemaining();
     }
 
+    // 从 channel 中读取 NetworkReceive 对象
     public long readFrom(ScatteringByteChannel channel) throws IOException {
         return readFromReadableChannel(channel);
     }
@@ -74,25 +79,34 @@ public class NetworkReceive implements Receive {
     // Need a method to read from ReadableByteChannel because BlockingChannel requires read with timeout
     // See: http://stackoverflow.com/questions/2866557/timeout-for-socketchannel-doesnt-work
     // This can go away after we get rid of BlockingChannel
+    // 从 Channel 中读取 NetworkReceive 的过程是
+    // 1. 先从 Channel 读取 4 字节放入 size 中, size.getInt() 得到具体内容的长度 L
+    // 2. 在申请一个长度为 L 字节的 ByteBuffer 为 buffer
+    // 3. 从 Channel 读取 L 字节的内容放入 buffer, NetworkReceive 对象就被填充好了
     @Deprecated
     public long readFromReadableChannel(ReadableByteChannel channel) throws IOException {
         int read = 0;
         if (size.hasRemaining()) {
+            // 从 channel 读取 size 数据, size 这个 ByteBuffer 已经强制为 4 bytes 大小
             int bytesRead = channel.read(size);
             if (bytesRead < 0)
                 throw new EOFException();
             read += bytesRead;
             if (!size.hasRemaining()) {
                 size.rewind();
+                // 从 size ByteBuffer 中读取整数, 即为后面具体内容的长度
                 int receiveSize = size.getInt();
                 if (receiveSize < 0)
                     throw new InvalidReceiveException("Invalid receive (size = " + receiveSize + ")");
+                // 如果从 size 读取出来的长度大于最大值则抛出异常
                 if (maxSize != UNLIMITED && receiveSize > maxSize)
                     throw new InvalidReceiveException("Invalid receive (size = " + receiveSize + " larger than " + maxSize + ")");
 
+                // 根据 size 得到的大小申请相应大小的  ByteBuffer 并继续从 channel 中读取具体内容
                 this.buffer = ByteBuffer.allocate(receiveSize);
             }
         }
+        // 继续从 channel 中读取内容放入 buffer 中
         if (buffer != null) {
             int bytesRead = channel.read(buffer);
             if (bytesRead < 0)
@@ -103,6 +117,7 @@ public class NetworkReceive implements Receive {
         return read;
     }
 
+    // payload() 就是返回存放具体内容的 buffer
     public ByteBuffer payload() {
         return this.buffer;
     }
